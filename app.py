28413b74bd3c21e01cbc3353fa67b7dbb6ffc9c8
Ibrahim Rasel <md.ibrahim.rasel@gmail.com>
2017-03-19 17:09:08 +0600

    Added a stylesheet, so index page shows all notes in the left side

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ class Note(db.Model):
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    notes = Note.query.all()
+    return render_template('home.html', notes=notes)
 
 if __name__ == "__main__":
     app.run()
