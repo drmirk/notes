dd42f9230c6ce222e0bedd2d72924d2158999834
Ibrahim Rasel <md.ibrahim.rasel@gmail.com>
2017-03-19 14:30:11 +0600

    Created basic database and table

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mydb.db"
+app.config['DEBUG'] = None
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String())
+    preview = db.Column(db.String())
+    body = db.Column(db.String())
+
+    def __repr__(self):
+        return "<Note {}>".format(self.title)
 
 @app.route('/')
 def home():
