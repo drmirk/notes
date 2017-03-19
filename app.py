dc9e1229217971479bdb0600f4e7f1b0488356f9
Ibrahim Rasel <md.ibrahim.rasel@gmail.com>
2017-03-19 22:21:40 +0600

    submit button stores text from ckeditor into database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -22,8 +22,13 @@ def home():
     notes = Note.query.all()
     return render_template('home.html', notes=notes)
 
-@app.route('/<int:note_id>')
+@app.route('/<int:note_id>', methods=['GET', 'POST'])
 def home2(note_id):
+    if(request.method == "POST"):
+        data = request.form['editor1']
+        single_note = Note.query.get(note_id)
+        single_note.body = data
+        db.session.commit()
     notes = Note.query.all()
     single_note = Note.query.get_or_404(note_id)
     return render_template('home2.html', notes=notes, single_note=single_note)
