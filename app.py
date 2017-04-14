7966a34d7b541291da4cbf506bb8bfa87d60ff5f
Ibrahim Rasel <md.ibrahim.rasel@gmail.com>
2017-04-15 00:44:50 +0600

    added a file manager for ckeditor

--- app.py
+++ app.py
@@ -212,6 +212,34 @@ def view_note(note_id):
     # rendering note from database
     return render_template('view_note.html', my_form=my_form, notes=notes, single_note=single_note)
 
+# File manager----------------------------
+import os
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = 'static/media/'
+ALLOWED_EXTENSIONS = set(['jpg', 'png', 'jpeg', 'bmp'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            funcNum = request.args.get('CKEditorFuncNum')
+            fileUrl = 'static/' + filename
+            print(fileUrl)
+            return render_template('file_browser.html', close_window=True, funcNum=funcNum, fileUrl=fileUrl)
+
+    return render_template('file_browser.html')
+#----------------------------------------------
 
 if __name__ == "__main__":
     app.run()
